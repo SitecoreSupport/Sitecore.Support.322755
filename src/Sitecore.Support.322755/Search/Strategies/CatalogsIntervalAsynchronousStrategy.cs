ea52ebb216187f775aaac4214185ee2572a66f5b
mao <mao@sitecore.net>
2019-04-09 11:05:20 +0300

    Override commerce interval asynchronous strategies

---
+++ src/Sitecore.Support.322755/Search/Strategies/CatalogsIntervalAsynchronousStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Commerce.Engine.Connect.Search;
+using Sitecore.Commerce.Plugin.Catalog;
+using Sitecore.Commerce.Plugin.ManagedLists;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.Support.Search.Strategies
+{
+  public class CatalogsIntervalAsynchronousStrategy : CatalogSystemIntervalAsynchronousStrategyBase<Catalog>
+  {
+    public CatalogsIntervalAsynchronousStrategy(string interval, string database, string maxMappingReloads, string itemsToIndexMinThreshold) 
+      : base(interval, database, maxMappingReloads, itemsToIndexMinThreshold)
+    {
+    }
+
+    protected override ManagedList GetIncrementalEntitiesToIndex(string environment, int skip)
+    {
+      Assert.ArgumentNotNullOrEmpty(environment, "environment");
+      return IndexUtility.GetCatalogsToIndex(environment, base.IncrementalIndexListName, skip, base.ItemsToTake);
+    }
+  }
+}
---
+++ src/Sitecore.Support.322755/Search/Strategies/CategoriesIntervalAsynchronousStrategy.cs
@@ -0,0 +1,25 @@
+﻿using Sitecore.Commerce.Engine.Connect.Search;
+using Sitecore.Commerce.Plugin.Catalog;
+using Sitecore.Commerce.Plugin.ManagedLists;
+using Sitecore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.Search.Strategies
+{
+  public class CategoriesIntervalAsynchronousStrategy : CatalogSystemIntervalAsynchronousStrategyBase<Category>
+  {
+    public CategoriesIntervalAsynchronousStrategy(string interval, string database, string maxMappingReloads, string itemsToIndexMinThreshold) 
+      : base(interval, database, maxMappingReloads, itemsToIndexMinThreshold)
+    {
+    }
+
+    protected override ManagedList GetIncrementalEntitiesToIndex(string environment, int skip)
+    {
+      Assert.ArgumentNotNullOrEmpty(environment, "environment");
+      return IndexUtility.GetCategoriesToIndex(environment, base.IncrementalIndexListName, skip, base.ItemsToTake);
+    }
+  }
+}
